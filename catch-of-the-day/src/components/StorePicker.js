719fea19c45e83e3e45216a5e59529c612cd3a4f
clarkngo <clarkngo@gmail.com>
2020-08-30 16:48:00 -0700

    first comp

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form className="store-selector">
+        { /* comment */ }
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Vist Store</button>
+      </form>
+    )
+  }
+}
+
+export default StorePicker;
